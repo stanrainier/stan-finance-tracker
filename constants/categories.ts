e290c52dbc701483280ad9174e4b2b81a740e353
stanrainier <stanrainiersalazar@gmail.com>
2025-09-21 23:57:13 +0800

    update categories

--- constants/categories.ts
+++ constants/categories.ts
@@ -2,17 +2,27 @@
 export const incomeCategories = [
   "Salary",
   "Business",
+  "Sale",
+  "Payment",
   "Investment",
   "Gift",
   "Bonus",
   "Interest",
+  "Payment",
+  "Refund",
+  "Allowance",
   "Other",
 ];
 
 export const expenseCategories = [
+  "Personal Expense",
   "Food",
+  "Withdrawal",
   "Transportation",
   "Bills",
+  "Online Payments",
+  "Loan Repayment",
+  "Load",
   "Shopping",
   "Health",
   "Entertainment",
